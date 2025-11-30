db311621630b8e8e6e536f235bdf30d5fea4b9fc
mansur-kl <shaikmansur625@gmail.com>
2025-11-30 07:35:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/success" element={<Success />} />
+      </Routes>
+    </BrowserRouter>
+
+  )
+}
+
+export default App
